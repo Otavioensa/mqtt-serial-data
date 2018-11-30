83aa753a4729f3267716a5121b6d84485fa56efd
Otavioensa <otavioensa@gmail.com>
2018-11-30 01:41:22 -0200

    application: add qos option
    add qos option when publishing a message

--- src/app.js
+++ src/app.js
@@ -7,19 +7,21 @@ const Readline = SerialPort.parsers.Readline
 const port = new SerialPort(portSrc)
 const parser = new Readline()
 
+const publishOptions = { qos: 1 }
+
 const client = mqtt.connect(broker)
 
 const onPublish = () => console.log('A message has been sent!')
 
 const onSerialData = (data) => {
-	console.log(data)
-	client.publish(topic, data, onPublish)
+  console.log(data)
+  client.publish(topic, data, publishOptions, onPublish)
 }
 
 const onBrokerConnection = () => {
-	console.log('connected')
-	port.pipe(parser)
-	parser.on('data', onSerialData)
+  console.log('connected')
+  port.pipe(parser)
+  parser.on('data', onSerialData)
 }
 
 client.on('connect', onBrokerConnection)
